3ba63b94277ca7fda8e10bdc7ac09250e23729c3
Mallerly01 <mallerly.carrasco2201@alumnos.ubiobio.cl>
2022-10-15 18:39:47 -0300

    avance clase cliente

--- Proyecto Semestral Version 1/Proyecto V1/src/Modelo/Cliente.java
+++ Proyecto Semestral Version 1/Proyecto V1/src/Modelo/Cliente.java
@@ -1,4 +1,44 @@
 package Modelo;
 
 public class Cliente {
+    private String rut;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private boolean activo=true;
+
+    public Cliente (String rut, String nom, String dir, String tel){
+        this.rut=rut;
+        this.nombre=nom;
+        this.direccion=dir;
+        this.telefono=tel;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+    public boolean isActivo(){
+        return activo;
+    }
+    public void setActivo(){
+        this.activo = true;
+        System.out.println("Activo");
+    }
+    public void setInactivo(){
+        this.activo = false;
+        System.out.println("Inactivo");
+    }
+
 }
