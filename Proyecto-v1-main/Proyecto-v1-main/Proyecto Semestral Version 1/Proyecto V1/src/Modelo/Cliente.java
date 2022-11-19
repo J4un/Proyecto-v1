d2813c764a802e3324dd3f38659dc25965ee6cf7
J4un <67522165+J4un@users.noreply.github.com>
2022-11-19 19:38:54 -0300

    Add files via upload

---
+++ Proyecto-v1-main/Proyecto-v1-main/Proyecto Semestral Version 1/Proyecto V1/src/Modelo/Cliente.java
@@ -0,0 +1,62 @@
+package Modelo;
+
+import java.util.ArrayList;
+
+public class Cliente {
+    private String rut;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private boolean activo=true;
+    private final ArrayList<Arriendo> arriendos;
+    public Cliente (String rut, String nom, String dir, String tel){
+        this.rut=rut;
+        this.nombre=nom;
+        this.direccion=dir;
+        this.telefono=tel;
+        arriendos=new ArrayList<>();
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+    public boolean isActivo(){
+
+        return activo;
+    }
+    public void setActivo(){
+        this.activo = true;
+    }
+    public void setInactivo(){
+        this.activo = false;
+    }
+    public void addArriendo(Arriendo arriendo){
+        arriendos.add(arriendo);
+    }
+    public Arriendo[] getArriendosPorDevolver(){
+        ArrayList<Arriendo> entregados=new ArrayList<Arriendo>();
+        for(Arriendo arriendo: arriendos){
+            if(arriendo.getEstado()==EstadoArriendo.ENTREGADO){
+                entregados.add(arriendo);
+            }
+        }
+        Arriendo [] listaEntregados = new Arriendo [entregados.size()];
+        for(int i=0;i<arriendos.size();i++){
+            listaEntregados[i]=entregados.get(i);
+        }
+        return listaEntregados;
+    }
+
+}
