f339091c27d776c52bba88498a6562a739e9d6a1
J4un <67522165+J4un@users.noreply.github.com>
2023-01-12 00:49:37 -0300

    Delete Proyecto V2 directory

--- Proyecto V2/src/Excepciones/ArriendoException.java
+++
@@ -1,7 +0,0 @@
-package Excepciones;
-
-public class ArriendoException extends Exception{
-    public ArriendoException(String msg){
-        super(msg);
-    }
-}
--- Proyecto V2/src/Vista/Main.java
+++
@@ -1,9 +0,0 @@
-package Vista;
-
-import Excepciones.ClienteException;
-
-public class Main {
-    public static void main(String[] args) throws ClienteException {
-        UIArriendoEquipos.getInstancia().menu();
-    }
-}
